ca9ab0888b397d10dc5b122b1592ecd4fe356437
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-19 10:50:35 -0600

    code design checked

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
+# Message Model
 class Message < ApplicationRecord
-  validates :message, :to_number, presence: :true
-  validates :to_number, :numericality => true
+  validates :message, :to_number, presence: true
+  validates :to_number, numericality: true
   belongs_to :provider, optional: true
 end
