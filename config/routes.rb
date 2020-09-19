5524663c0e6360707fc904daba679dc0cf5a3438
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-19 08:06:30 -0600

    message controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post '/message', to: 'messages#create'
   post '/update_message', to: 'messages#update'
-  get '/messages', to: 'messages#index'
+  get '/message', to: 'messages#index'
   get '/provider', to: 'providers#index'
   post '/provider', to: 'providers#create'
   post '/delete_provider', to: 'providers#delete'
